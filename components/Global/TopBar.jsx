909b8ff50640307ae388308eab5f94c2430e7f0b
youssef-saidi <youssefsaidi869@gmail.com>
2023-11-08 13:52:09 +0100

    final version

--- components/Global/TopBar.jsx
+++ components/Global/TopBar.jsx
@@ -54,7 +54,7 @@ const TopBar = (props) => {
 
         <div className="absolute right-0 flex select-none">
           <div className="flex items-center">
-            {/* <button>
+             <button>
               <svg
                 className="mx-3"
                 width="20"
@@ -73,24 +73,6 @@ const TopBar = (props) => {
               </svg>
             </button>
 
-            <button>
-              <svg
-                className="mx-3"
-                width="20"
-                height="16"
-                viewBox="0 0 20 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1 4L8.8906 9.2604C9.5624 9.70827 10.4376 9.70827 11.1094 9.2604L19 4M3 15H17C18.1046 15 19 14.1046 19 13V3C19 1.89543 18.1046 1 17 1H3C1.89543 1 1 1.89543 1 3V13C1 14.1046 1.89543 15 3 15Z"
-                  stroke="#5D6E81"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </button> */}
 
             <button>
               <svg
